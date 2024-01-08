df708eba25e28bb23186c50c5c80bc3bab85ec12
aespallargas <3933314+aespallargas@users.noreply.github.com>
2024-01-08 11:15:28 +0100

    first commit

---
+++ src/data/products.js
@@ -0,0 +1,242 @@
+export const products = [
+    {
+      "name": "Sudadera Gris",
+      "price": 49,
+      "availability": 5,
+      "category": 1
+    },
+    {    
+      "name": "Sudadera Naranja Hombre",
+      "price": 29,
+      "availability": 3,
+      "category": 1
+    },
+    {    
+      "name": "Sudadera Azul",
+      "price": 49,
+      "availability": 8,
+      "category": 1
+    },
+    {    
+      "name": "Sudadera Blanca",
+      "price": 29,
+      "availability": 10,
+      "category": 1
+    },
+    {    
+      "name": "Sudadera Negra / Naranja",
+      "price": 49,
+      "availability": 10,
+      "category": 1
+    },
+    {    
+      "name": "Sudadera Negra / Rojo",
+      "price": 39,
+      "availability": 10,   
+      "category": 1
+    },
+    {    
+      "name": "Sudadera Rojo",
+      "price": 49,
+      "availability": 10,
+      "category": 1
+    },
+    {    
+      "name": "Sudadera Blanco / Azul",
+      "price": 49,
+      "availability": 10,
+      "category": 1
+    },
+    {    
+      "name": "Sudadera Amarillo Mujer",
+      "price": 39,
+      "availability": 10,
+      "category": 1
+    },
+    {
+      "name": "Bambas Mujer",
+      "price": 59,
+      "availability": 10,   
+      "category": 2
+    },
+    {
+      "name": "Zapato Casual",
+      "price": 49,
+      "availability": 10,   
+      "category": 2
+    },
+    {
+      "name": "Bambas Mujer Rosa",
+      "price": 49,
+      "availability": 10,   
+      "category": 2
+    },
+    {
+      "name": "Bambas Gris",
+      "price": 79,
+      "availability": 10,   
+      "category": 2
+    },
+    {
+      "name": "Bambas Rojo / Negro",
+      "price": 69,
+      "availability": 10,   
+      "category": 2
+    },
+    {
+      "name": "Bambas Mujer Deporte",
+      "price": 69,
+      "availability": 10,   
+      "category": 2
+    },
+    {
+      "name": "Bambas Azul Hombre",
+      "price": 69,
+      "availability": 10,   
+      "category": 2
+    },
+    {
+      "name": "Bambas Azul",
+      "price": 69,
+      "availability": 10,   
+      "category": 2
+    },
+    {
+      "name": "Bambas Casual Hombre",
+      "price": 59,
+      "availability": 10,   
+      "category": 2
+    },
+    {
+      "name": "Bambas Casual Gris",
+      "price": 49,
+      "availability": 10,   
+      "category": 2
+    },
+    {
+      "name": "Bambas Bota Claros",
+      "price": 59,
+      "availability": 10,   
+      "category": 2
+    },
+    {
+      "name": "Bambas Bota Negro",
+      "price": 59,
+      "availability": 10,   
+      "category": 2
+    },
+    {
+      "name": "Bambas Bota Negro / Azul",
+      "price": 59,
+      "availability": 10,   
+      "category": 2
+    },
+    {
+      "name": "Bambas Casual Negro",
+      "price": 49,
+      "availability": 10,   
+      "category": 2
+    },
+    {
+      "name": "Bambas Casual Gris / Negro",
+      "price": 49,
+      "availability": 10,   
+      "category": 2
+    },
+    {
+      "name": "Bambas Hombre Negro",
+      "price": 59,
+      "availability": 10,   
+      "category": 2
+    },
+    {
+      "name": "Bambas Hombre Gris",
+      "price": 69,
+      "availability": 10,   
+      "category": 2
+    },
+    {
+      "name": "Zapato Gris Hombre",
+      "price": 69,
+      "availability": 10,   
+      "category": 2
+    },
+    {
+      "name": "Bambas Gris / Rojo",
+      "price": 49,
+      "availability": 10,
+      "category": 2
+    },
+    {
+      "name": "Bambas Negro Mujer",
+      "price": 59,
+      "availability": 10,   
+      "category": 2
+    },
+    {
+      "name": "Bambas Azul Casual",
+      "price": 69,
+      "availability": 10,   
+      "category": 2
+    },
+    {
+      "name": "Bambas Deporte Gris",
+      "price": 69,
+      "availability": 10,   
+      "category": 2
+    },
+    {
+      "name": "Bambas Verde Casual",
+      "price": 59,
+      "availability": 10,   
+      "category": 2
+    },
+    {
+      "name": "Bambas Rojo Casual",
+      "price": 59,
+      "availability": 10,   
+      "category": 2
+    },
+    {
+      "name": "Gafas Sol - Gris",
+      "price": 39,
+      "availability": 10,   
+      "category": 3
+    },
+    {
+      "name": "Gafas Sol - Negro",
+      "price": 39,
+      "availability": 10,   
+      "category": 3
+    },
+    {
+      "name": "Gafas Oscuros",
+      "price": 29,
+      "availability": 10,
+      "category": 3
+    },
+    {
+      "name": "Gafas para Sol",
+      "price": 49,
+      "availability": 10,
+      "category": 3
+    },
+    {
+      "name": "Gafas Sol acabado azul",
+      "price": 39,
+      "availability": 10,
+      "category": 3
+    },
+    {
+      "name": "Gafas Modernos Negros",
+      "price": 39,
+      "availability": 10,
+      "category": 3
+    },
+    {
+      "name": "Gafas Clasicos",
+      "price": 39,
+      "availability": 10,
+      "category": 3
+    }
+  ]
